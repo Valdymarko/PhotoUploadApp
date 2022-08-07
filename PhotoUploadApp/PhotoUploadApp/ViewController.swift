0b5d5333fb62278b434e1437a18a4061fdaba6d9
Volodymyr Ilkiv <volodya.ilkiv@gmail.com>
2022-08-07 12:50:24 +0300

    Initial commit

---
+++ PhotoUploadApp/PhotoUploadApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoUploadApp
+//
+//  Created by Volodymyr Ilkiv on 07.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
